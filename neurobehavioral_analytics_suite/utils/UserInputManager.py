3e87da2b07dc20ece783ccc2ebfcdaba0cf95cd1
Lane <lane@seattleu.edu>
2024-06-05 14:50:44 -0700

    load_data test command

--- neurobehavioral_analytics_suite/utils/UserInputManager.py
+++ neurobehavioral_analytics_suite/utils/UserInputManager.py
@@ -14,8 +14,11 @@ Maintainer: Lane
 Email: justlane@uw.edu
 Status: Prototype
 """
+import dask.distributed
+
 from neurobehavioral_analytics_suite.machine_learning.MachineLearning import MachineLearning
 from neurobehavioral_analytics_suite.operation_manager.operations.CustomOperation import CustomOperation
+from neurobehavioral_analytics_suite.operation_manager.operations.computation.DaskOperation import DaskOperation
 from neurobehavioral_analytics_suite.operation_manager.operations.persistent.ResourceMonitorOperation import (
     ResourceMonitorOperation)
 from neurobehavioral_analytics_suite.utils.ErrorHandler import ErrorHandler
@@ -59,16 +62,32 @@ class UserInputManager:
             await self.operation_control.stop_all_operations()
             return "UserInputManager.process_user_input: Stopping all operations..."
 
+        elif user_input == "load_data":
+            def load_data():
+                import dask.dataframe as dd
+                df = dd.read_csv("../sample_datasets/2024-Tariq-et-al_olfaction/8-30-2021-2-08 "
+                                 "PM-Mohammad-ETHSensor-CB3-3_reencodedDLC_resnet50_odor"
+                                 "-arenaOct3shuffle1_200000_filtered.csv")
+                return df.compute()
+
+            await self.operation_control.operation_manager.add_operation(
+                operation_type=DaskOperation, error_handler=self.error_handler, func=load_data,
+                name="LoadData", local_vars=self.operation_control.local_vars, client=dask.distributed.Client())
+
         elif user_input == "machine_learning":
-            ml_operation = MachineLearning(operation_control=self.operation_control, error_handler=self.error_handler)
-            result = await ml_operation.extract_data(file_path="../../../sample_datasets/2024-Tariq-et-al_olfaction/8-30-2021-2-08 PM-Mohammad-ETHSensor-CB3-3_reencodedDLC_resnet50_odor-arenaOct3shuffle1_200000_filtered.csv")
-            print(result)
-            self.logger.info("UserInputManager.process_user_input: Attached machine learning...")
-            # data = await ml_operation.extract_data(file_path="../../sample_datasets/2024-Tariq-et-al_olfaction/8-30
+            self.logger.error("UserInputManager.process_user_input: Machine Learning not implemented.")
+            return "UserInputManager.process_user_input: Machine Learning not implemented."
+
+            # ml_operation = MachineLearning(operation_control=self.operation_control,
+            # error_handler=self.error_handler) result = await ml_operation.extract_data(
+            # file_path="../../../sample_datasets/2024-Tariq-et-al_olfaction/8-30-2021-2-08
+            # PM-Mohammad-ETHSensor-CB3-3_reencodedDLC_resnet50_odor-arenaOct3shuffle1_200000_filtered.csv") print(
+            # result) self.logger.info("UserInputManager.process_user_input: Attached machine learning...") data =
+            # await ml_operation.extract_data(file_path="../../sample_datasets/2024-Tariq-et-al_olfaction/8-30
             # -2021-2-08 PM-Mohammad-ETHSensor-CB3-3_reencodedDLC_resnet50_odor-arenaOct3shuffle1_200000_filtered.csv
             # ") train_data, test_data = ml_operation.split_data(data, 7, test_size=0.2)
 
-            return f"UserInputManager.process_user_input: Attached machine learning..."
+            # return f"UserInputManager.process_user_input: Attached machine learning..."
 
         elif user_input == "pause":
             await self.operation_control.pause_all_operations()
