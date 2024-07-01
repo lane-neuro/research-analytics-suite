0ee73a4eed5290807c9790f9871d1ce71dbb0bd5
Lane <lane@seattleu.edu>
2024-06-30 21:33:14 -0700

    Update __main__.py

--- research_analytics_suite/__main__.py
+++ research_analytics_suite/__main__.py
@@ -45,6 +45,6 @@ def main():
 
 if __name__ == '__main__':
     sys.argv = ['__main__.py',
-                '-n', "RAS-test",
+                '-n', "RAS-test-workspace",
                 ]
     main()
